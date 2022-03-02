83d550679b0cd4dcd9f01603f75c1a6d2091a6c8
mercedes6115 <89904399+mercedes6115@users.noreply.github.com>
2022-03-02 06:10:35 +0000

    day01

--- day01.java
+++ day01.java
@@ -3,7 +3,18 @@ package day01;
 public class Day01 { 
   
 	public static void main(String[] args) {
-    System.out.println("Hello")
+    // char -> 문자 하나를 저장하는 자료형 ""를 사용
+    char a ='A';
+    char b = 65; //아스키 코드값을 문자로 인식
+    char c = '\u0041'; // 유니코드값을 문자로 인식
+    char value1 = 'A';
+    char value2 = '가';
+    char value3 = '3'; // 숫자가 아닌 문자 3 
+
+    System.out.println(value1);
+    System.out.println(value2);
+    System.out.println(value3);
+    System.out.println(b);
 
 	}
 
